2e6ddf2282162d21c6c5792c34698af38987d3cf
wsr_wang <wsr_wang@163.com>
2016-04-26 18:57:54 +0800

    ceshi

--- src/main/java/testgit/helloWorld.java
+++ src/main/java/testgit/helloWorld.java
@@ -5,7 +5,7 @@ public class helloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("git");
+		System.out.println("git");//helloworld
 		
 	}
 
