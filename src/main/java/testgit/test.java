59078e79dddd42633538742fa19b30a054f36025
wsr_w <wsr_w@DESKTOP-OB6JR1M>
2018-08-04 19:20:53 +0800

    让人

---
+++ src/main/java/testgit/test.java
@@ -0,0 +1,14 @@
+package testgit;
+/**
+* @author 作者
+* @version 创建时间：2018年8月4日 下午7:14:26
+* 类说明
+*/
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
